8fe0ea093d4cb6a4e74bbd196e99d1b0a87e1eba
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-04-27 16:06:58 +0200

    Initial screen setup for each app screen

---
+++ screens/MealDetailsScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+export default function () {
+    return (
+        <View>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+
+});

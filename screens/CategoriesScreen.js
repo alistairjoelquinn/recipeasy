c590fa0a5f1a355c75b750b8e2ade63a270c46b7
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-04-27 16:10:57 +0200

    intial styling added to each of the screens

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
-export default function () {
+export default function CategoriesScreen() {
     return (
-        <View>
-
+        <View style={styles.screen}>
+            <Text>The Categories Screen</Text>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
 });
--- screens/FavouritesScreen.js
+++ screens/FavouritesScreen.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
-export default function () {
+export default function FavouritesScreen() {
     return (
-        <View>
-
+        <View style={styles.screen}>
+            <Text>The Favourite Screen</Text>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
 });

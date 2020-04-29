7a3f046672d79f36133a8c010c74a4bcf88b6802
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-04-29 15:19:30 +0200

    enabled react native screens and removed app..js clutter

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font';
 import { AppLoading } from 'expo';
+import { enableScreens } from 'react-native-screens';
 
 import MealsNavigator from './navigation/MealsNavigator';
+
+enableScreens();
 
 const fetchFonts = () => {
     return Font.loadAsync({
@@ -29,12 +31,3 @@ export default function App() {
         <MealsNavigator />
     );
 };
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});

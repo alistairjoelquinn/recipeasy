ba51c178943d15b488978f9e156d0f8ab02bf656
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-04-29 18:04:29 +0200

    meal item component completed except central align not perfect

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, ImageBackground } from 'react-native';
 
 export default function MealItem({ itemData, onSelect }) {
     return (
@@ -7,10 +7,24 @@ export default function MealItem({ itemData, onSelect }) {
             <TouchableOpacity onPress={onSelect}>
                 <View>
                     <View style={{ ...styles.mealRow, ...styles.mealHeader }}>
-                        <Text>{itemData.item.title}</Text>
+                            <ImageBackground 
+                                source={{uri: itemData.item.imageUrl}} 
+                                style={styles.bgImage}
+                            >
+                                <View style={styles.titleContainer}>
+                                    <Text 
+                                        style={styles.title}
+                                        numberOfLines={1}
+                                    >   
+                                        {itemData.item.title}
+                                    </Text>
+                                </View>
+                            </ImageBackground>
                     </View>
                     <View style={{ ...styles.mealRow, ...styles.mealDetail }}>
                         <Text>{itemData.item.duration} minutes</Text>
+                        <Text>{itemData.item.complexity.toUpperCase()}</Text>
+                        <Text>{itemData.item.affordability.toUpperCase()}</Text>
                     </View>
                 </View>
             </TouchableOpacity>
@@ -22,16 +36,39 @@ const styles = StyleSheet.create({
     mealRow: {
         flexDirection: 'row'
     },
+    bgImage: {
+        height: '100%',
+        width: '100%',
+        justifyContent: 'flex-end'
+    },
     mealItem: {
         height: 200,
-        width: '100%',
-        backgroundColor: '#ccc'
+        width: '95%',
+        backgroundColor: '#f5f5f5',
+        borderRadius: 10,
+        overflow: 'hidden',
+        margin: 10,
+        borderWidth: 1,
+        borderColor: '#ccc',
     },
     mealHeader: {
-        height: '90%',
+        height: '85%'
     },
     mealDetail: {
         paddingHorizontal: 10,
-        justifyContent: 'space-between'
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        height: '15%'
+    },
+    titleContainer: {
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        paddingVertical: 5,
+        paddingHorizontal: 12,
+    },
+    title: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 20,
+        color: 'white',
+        textAlign: 'center'
     }
 });

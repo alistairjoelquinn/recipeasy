be737b731c5e7f96e348b19aaa4948167dda57d7
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-04-30 14:30:16 +0200

    stack navigation passed in favourites props to take user to meal in favourites stack upon touch

--- screens/FavouritesScreen.js
+++ screens/FavouritesScreen.js
@@ -3,12 +3,13 @@ import React from 'react';
 import { MEALS } from '../data/test-data';
 import MealList from '../components/MealList';
 
-export default function FavouritesScreen() {
+export default function FavouritesScreen({ navigation }) {
     const favMeals = MEALS.filter(meal => meal.id === 'm1' || meal.id === 'm2');
 
     return (
         <MealList 
             displayMeals={favMeals}
+            navigation={navigation}
         />
     );
 }

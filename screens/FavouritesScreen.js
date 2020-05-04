aa836b6480ab0118346de4f2a0314e7d1ec8a7a9
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-05-04 17:27:42 +0200

    text default component imported and used in favourites screen

--- screens/FavouritesScreen.js
+++ screens/FavouritesScreen.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { useSelector } from 'react-redux';
 
 import MealList from '../components/MealList';
-import CustomHeaderButton from '../components/CustomHeaderButton'
+import CustomHeaderButton from '../components/CustomHeaderButton';
+import TextDefault from '../components/TextDefault';
 
 export default function FavouritesScreen({ navigation }) {
     const favMeals = useSelector(state => state.meals.favouriteMeals);
+
+    if(favMeals.length === 0 || !favMeals) {
+        return <View style={styles.content}>
+            <TextDefault>No Favourites So Far! Start Adding Some...</TextDefault>
+        </View>
+    }
 
     return (
         <MealList 
@@ -33,3 +41,11 @@ FavouritesScreen.navigationOptions = navData => {
         }
     );
 };
+
+const styles = StyleSheet.create({
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
